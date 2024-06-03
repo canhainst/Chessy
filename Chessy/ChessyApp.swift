32a49d74db8acb4768e03e0263cbbd1cfab629f1
nguyenthanh <tanthanh191003@gmail.com>
2024-06-03 17:56:25 +0700

    Initial Commit

---
+++ Chessy/ChessyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessyApp.swift
+//  Chessy
+//
+//  Created by Nguyễn Thành on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChessyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chessy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chessy
+//
+//  Created by Nguyễn Thành on 03/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
